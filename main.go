f1b0e9299f32e2132c45c9b17be6639d823d969a
Issac Trotts <issactrotts@google.com>
2018-05-29 08:52:46 +0800

    Keep a histogram and directly update it.
    This simplifies the code a bit.

--- main.go
+++ main.go
@@ -23,15 +23,9 @@ var interval = time.Second
 
 var reqQPS int32 = 1
 
-// An event
-type event struct {
-	t0         time.Time
-	t1         time.Time
-	statusText string
-}
-
-// This chan is meant to have enough capacity for what can happen in a second.
-var eventChan = make(chan event)
+// This is a histogram of events over the past second.
+var hmu sync.Mutex
+var histogram = make(map[string]int)
 
 func main() {
 	flag.Parse()
@@ -52,8 +46,6 @@ func main() {
 
 	go hammer(u)
 	go sendTermboxInterrupts()
-	go addToEventSlice()
-	go removeOldEvents()
 
 	for {
 		switch ev := termbox.PollEvent(); ev.Type {
@@ -93,26 +85,26 @@ func hammer(url string) {
 
 func worker(ch chan string) {
 	for u := range ch {
-		t0 := time.Now()
 		switch *fetcher {
 		case "curl":
 			cmd := exec.Command("curl", "-s", "-S", u)
 			out, _ := cmd.CombinedOutput()
-			eventChan <- event{t0, time.Now(), string(out)}
+			addToHistogram(string(out))
 		case "go":
 			client := http.Client{Timeout: time.Duration(time.Second)}
 			resp, err := client.Get(u)
 			if resp != nil {
 				// Read it, just in case that matters somehow.
 				if _, err := ioutil.ReadAll(resp.Body); err != nil {
-					eventChan <- event{t0, time.Now(), fmt.Sprintf("Failed to read response body: %v", err)}
+					addToHistogram(fmt.Sprintf("Failed to read response body: %v", err))
 					continue
 				}
 				if err := resp.Body.Close(); err != nil {
-					eventChan <- event{t0, time.Now(), fmt.Sprintf("Failed to close response body: %v", err)}
+					addToHistogram(fmt.Sprintf("Failed to close response body: %v", err))
 					continue
 				}
 			}
+			// status text
 			var st string
 			if err != nil {
 				parts := strings.Split(err.Error(), ": ")
@@ -120,13 +112,31 @@ func worker(ch chan string) {
 			} else {
 				st = http.StatusText(resp.StatusCode)
 			}
-			eventChan <- event{t0, time.Now(), st}
+			addToHistogram(st)
 		case "noop":
-			eventChan <- event{t0, time.Now(), "Did nothing."}
+			addToHistogram("Did nothing")
 		default:
-			eventChan <- event{t0, time.Now(), fmt.Sprintf("Unrecognized value for --fetcher: %q\n", *fetcher)}
+			dief(fmt.Sprintf("Unrecognized value for --fetcher: %q\n", *fetcher))
 		}
 	}
+}
+
+func addToHistogram(s string) {
+	hmu.Lock()
+	defer hmu.Unlock()
+	histogram[s]++
+	go func() {
+		<-time.After(time.Second)
+		hmu.Lock()
+		defer hmu.Unlock()
+		histogram[s]--
+	}()
+}
+
+func dief(fmat string, args ...interface{}) {
+	termbox.Close()
+	fmt.Printf(fmat, args...)
+	os.Exit(1)
 }
 
 func ticktock() chan struct{} {
@@ -141,14 +151,13 @@ func ticktock() chan struct{} {
 }
 
 func sendTermboxInterrupts() {
-	for _ = range time.Tick(100 * time.Millisecond) {
+	for _ = range time.Tick(500 * time.Millisecond) {
 		termbox.Interrupt()
 	}
 }
 
 // draw repaints the termbox UI, showing stats.
 func draw() {
-	keys, m, maxLatency := makeReport()
 
 	// Do the actual drawing.
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
@@ -158,15 +167,20 @@ func draw() {
 	tbprint(0, y, termbox.ColorWhite, termbox.ColorBlack, fmt.Sprintf("%d workers", *numWorkers))
 	y++
 	y++
-	tbprint(0, y, termbox.ColorWhite, termbox.ColorBlack, fmt.Sprintf("Max latency: %v", maxLatency))
-	y++
-	if len(m) == 0 {
+	hmu.Lock()
+	defer hmu.Unlock()
+	if len(histogram) == 0 {
 		tbprint(0, y, termbox.ColorWhite, termbox.ColorBlack, fmt.Sprintf("No responses in past %v", interval))
 	} else {
 		tbprint(0, y, termbox.ColorWhite, termbox.ColorBlack, fmt.Sprintf("Responses in past %v:", interval))
 		y++
+		var keys []string
+		for k := range histogram {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
 		for _, k := range keys {
-			msg := fmt.Sprintf("  %s: %d", k, m[k])
+			msg := fmt.Sprintf("  %s: %d", k, histogram[k])
 			tbprint(0, y, termbox.ColorWhite, termbox.ColorBlack, msg)
 			y++
 		}
@@ -179,54 +193,4 @@ func tbprint(x, y int, fg, bg termbox.Attribute, msg string) {
 		termbox.SetCell(x, y, c, fg, bg)
 		x++
 	}
-}
-
-var emu sync.Mutex
-var events []event
-
-func addToEventSlice() {
-	for e := range eventChan {
-		emu.Lock()
-		events = append(events, e)
-		emu.Unlock()
-	}
-}
-
-func removeOldEvents() {
-	for _ = range time.Tick(10 * time.Millisecond) {
-		now := time.Now()
-		emu.Lock()
-		events2 := make([]event, 0, len(events))
-		for _, e := range events {
-			if now.Sub(e.t0) < interval {
-				events2 = append(events2, e)
-			}
-		}
-		events = events2
-		emu.Unlock()
-	}
-}
-
-func makeReport() (keys []string, m map[string]int, maxLatency time.Duration) {
-	m = make(map[string]int)
-	now := time.Now()
-	emu.Lock()
-	for _, e := range events {
-		if now.Sub(e.t0) < interval {
-			m[e.statusText]++
-			dt := e.t1.Sub(e.t0)
-			if dt > maxLatency {
-				maxLatency = dt
-			}
-		}
-	}
-	emu.Unlock()
-
-	// Get the map keys and sort them
-	for k := range m {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-
-	return keys, m, maxLatency
 }
